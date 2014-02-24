3c7b222f321710da804ea46c90d49a85bd2de4a8
devnexus_user <nkriegel@thoughtworks.com>
2014-02-24 03:23:23 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Devnexus::Application.config.secret_key_base = '47002ccafffe58538eeae1e1eb548558ed4d13f894d38fbdd76f5d511bbb9270878e42f885dde6868418dadf96975652552e0f7f60b819fc29dba2b380c8e7cc'
