0695b18c55db6c82f50f510170ce1fad65edbc37
tworker <tworker@onyx.ove.com>
2014-02-24 17:28:40 -0500

    green my test

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,5 +1,6 @@
 class CalculatorController < ApplicationController
   def calculate
+    @result = 'error' if params[:op1].match(/^\d+$/).nil? || params[:op2].match(/^\d+$/).nil?
     @result = params[:op1].to_i * params[:op2].to_i if params[:op1]
   end
 end
